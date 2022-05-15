487f1256a3eb2596128c2fb7215ab78265fe2e82
Demid <dagdog2506@gmail.com>
2022-05-15 15:24:07 +0500

    fix bugs

--- string_sum.go
+++ string_sum.go
@@ -15,6 +15,50 @@ var (
 	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
 )
 
+func StringSum(input string) (output string, err error) {
+
+	if len(input) == 0 {
+		return "", fmt.Errorf("%w", errorEmptyInput)
+	}
+
+	if countOperands(input) > 2 || countOperands(input) == 0 {
+		return "", fmt.Errorf("%w", errorNotTwoOperands)
+	}
+	input = stringToFormat(input)
+
+	numbers := make([]string, 0)
+	q := 1
+
+	if strings.Count(input, "+") >= 1 {
+		numbers = strings.Split(input, "+")
+	}
+	if strings.Count(input, "-") > 1 {
+		numbers = strings.Split(input, "-")
+
+		q = -1
+	}
+
+	if strings.Count(input, "-") == 1 {
+		symbol := "-"
+		numbers = strings.Split(input, "-")
+		numbers[1] = symbol + numbers[1]
+	}
+
+	sum := 0
+	for _, number := range numbers {
+		num := strings.ReplaceAll(number, " ", "")
+
+		item, err := strconv.Atoi(num)
+		if err != nil {
+			return "", fmt.Errorf("Invalid format or less arguments: %w", err)
+		}
+		sum += item
+	}
+
+	return strconv.Itoa(sum * q), nil
+
+}
+
 func stringToFormat(str string) string {
 	strWithoutSpaces := strings.ReplaceAll(str, " ", "")
 	if strings.HasPrefix(strWithoutSpaces, "+") {
@@ -28,41 +72,4 @@ func countOperands(str string) int {
 	count += strings.Count(str, "+")
 	count += strings.Count(str, "-")
 	return count
-}
-
-func StringSum(input string) (output string, err error) {
-
-	if len(input) == 0 {
-		return "", fmt.Errorf("%w", errorEmptyInput)
-	}
-
-	if countOperands(input) != 2{
-		return "", fmt.Errorf("%w", errorNotTwoOperands)
-	}
-
-	formatedInput := stringToFormat(input)
-
-	numbers := make([]string, 0)
-	coefficient := 1
-
-	if strings.Count(formatedInput, "+") >= 1 {
-		numbers = strings.Split(formatedInput, "+")
-	} else if strings.Count(formatedInput, "-") > 1 {
-		str := strings.Replace(formatedInput, "-", "", 1)
-		numbers = strings.Split(str, "-")
-		coefficient = -1
-	}
-
-	sum := 0
-	for _, number := range numbers {
-		num := strings.ReplaceAll(number, " ", "")
-		item, err := strconv.Atoi(num)
-		if err != nil {
-			return "", fmt.Errorf("syntax err: %w", err)
-		}
-		sum += item
-	}
-
-	return strconv.Itoa(sum * coefficient), nil
-
 }
