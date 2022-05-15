52ec99b6c83500965b59f710e933791dbfa4fd03
Demid <dagdog2506@gmail.com>
2022-05-15 14:43:57 +0500

    Done.

--- string_sum.go
+++ string_sum.go
@@ -15,16 +15,6 @@ var (
 	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
 )
 
-// Implement a function that computes the sum of two int numbers written as a string
-// For example, having an input string "3+5", it should return output string "8" and nil error
-// Consider cases, when operands are negative ("-3+5" or "-3-5") and when input string contains whitespace (" 3 + 5 ")
-//
-//For the cases, when the input expression is not valid(contains characters, that are not numbers, +, - or whitespace)
-// the function should return an empty string and an appropriate error from strconv package wrapped into your own error
-// with fmt.Errorf function
-//
-// Use the errors defined above as described, again wrapping into fmt.Errorf
-
 func StringSum(input string) (output string, err error) {
 
 	if len(input) == 0 {
@@ -34,25 +24,40 @@ func StringSum(input string) (output string, err error) {
 	if countOperands(input) != 2 {
 		return "", fmt.Errorf("%w", errorNotTwoOperands)
 	}
+	input = stringToFormat(input)
 
-	numbers := make([]string,0)
+	numbers := make([]string, 1)
+	q := 1
 
-	if strings.Count(input,"+") == 0 {
+	if strings.Count(input, "+") >= 1 {
 		numbers = strings.Split(input, "+")
-	} else {
-		numbers = strings.Split(input, "-")
+	} else if strings.Count(input, "-") > 1 {
+		str := strings.Replace(input, "-", "", 1)
+		numbers = strings.Split(str, "-")
+		q = -1
 	}
 
 	sum := 0
 	for _, number := range numbers {
-		item, err := strconv.Atoi(strings.TrimSpace(number))
-		if err != nil{
+		num := strings.ReplaceAll(number, " ", "")
+
+		item, err := strconv.Atoi(num)
+		if err != nil {
 			return "", fmt.Errorf("syntax err: %w", err)
 		}
 		sum += item
 	}
 
-	return "", nil
+	return strconv.Itoa(sum * q), nil
+
+}
+
+func stringToFormat(str string) string {
+	strWithoutSpaces := strings.ReplaceAll(str, " ", "")
+	if strings.HasPrefix(strWithoutSpaces, "+") {
+		strWithoutSpaces = strWithoutSpaces[1:]
+	}
+	return strWithoutSpaces
 }
 
 func countOperands(str string) int {
